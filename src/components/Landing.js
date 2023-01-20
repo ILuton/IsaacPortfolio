6774cefa016837e8957fdd4e6f0e314d0a03496c
Isaac Luton <isaacdluton@gmail.com>
2023-01-20 07:28:20 -0800

    remove start files. add folders for file layout. add landing page background.

---
+++ src/components/Landing.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "../css/Landing.css"
+
+function Landing() {
+  return (
+    <div className='landingContainer'>
+        <div className='landingName'>
+            <h1>Isaac Luton</h1>
+            <h2>Full-stack Web Developer.</h2>
+        </div>
+        <div className='landingLearnMore'>
+            <h3>Learn more</h3>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Landing
