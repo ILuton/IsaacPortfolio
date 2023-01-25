9d2e999504f58224faccff760b7b355d6f11ad4f
Isaac Luton <isaacdluton@gmail.com>
2023-01-25 09:35:07 -0800

    hamburger menu link positioning

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,24 +1,40 @@
-import { bubble as Menu } from 'react-burger-menu'
-import { useState } from 'react'
-import "../css/Hamburger.css"
-import github from "../images/github.png"
-import linkedin from "../images/linkedin.png"
+import { bubble as Menu } from "react-burger-menu";
+import { useState } from "react";
+import "../css/Hamburger.css";
+import github from "../images/github.png";
+import linkedin from "../images/linkedin.png";
 
 function Hamburger() {
-  const [menuOpen, setMenuOpen] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <Menu isOpen={menuOpen} onStateChange={(state) => setMenuOpen(state.isOpen)}>
-      <a id="home" className="menu-item" href="/">Home</a>
-      <a id="about" className="menu-item" href="/about">About</a>
-      <a id="work" className="menu-item" href="/work">My Work</a>
-      <a id="contact" className="menu-item" href="/contact">Contact</a>
-      <div className='burgerLinks'>
-      <a href='./'><img className="burgerGithub" src={github} alt=""/></a>
-      <a href='./'><img className="burgerLinkedIn"src={linkedin} alt=""/></a>
+    <Menu
+      isOpen={menuOpen}
+      onStateChange={(state) => setMenuOpen(state.isOpen)}
+    >
+      <a id="home" className="menu-item" href="/">
+        Home
+      </a>
+      <a id="about" className="menu-item" href="/about">
+        About
+      </a>
+      <a id="work" className="menu-item" href="/work">
+        My Work
+      </a>
+      <a id="contact" className="menu-item" href="/contact">
+        Contact
+      </a>
+
+      <div className="burgerLinks">
+        <a href="./">
+          <img className="burgerGithub" src={github} alt="" />
+        </a>
+        <a href="./">
+          <img className="burgerLinkedIn" src={linkedin} alt="" />
+        </a>
       </div>
     </Menu>
   );
 }
 
-export default Hamburger
+export default Hamburger;
